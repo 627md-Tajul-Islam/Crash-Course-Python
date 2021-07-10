10e8ab9a93b84a049366e1cf4996e5d828754d7c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 11:52:50 -0700

    Data Types added

---
+++ Data Types.py
@@ -0,0 +1,34 @@
+# Different Types of Data
+# Numeric Data Type
+x = 5
+result = type(x)
+print(result)
+
+y = 5.5
+result = type(y)
+print(result)
+
+z = 5+7j
+result = type(z)
+print(result)
+
+a = 5
+b = 10
+result = type(a>b)
+print(result)
+
+c = 'Tajul Islam'
+result = type(c)
+print(result)
+
+d = [1,2,3,4,5,6,7,8,9,10]
+result = type(d)
+print(result)
+
+e = (1,2,3,4,5,6,7,8,9,10)
+result = type(e)
+print(result)
+
+f = {1,2,3,4,5,6,7,8,9,10}
+result = type(f)
+print(result)
