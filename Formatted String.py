7a5af019da27c59f75fca422494da3b62a0d807a
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 12:03:01 -0700

    Formatted String added

---
+++ Formatted String.py
@@ -0,0 +1,12 @@
+#num = 20.5
+#string =  "Tajul"
+#bool = True
+#print(type(True))
+'''
+num1 = 20
+num2 = 30
+print(f" {num1} + {num2} = {num1+num2} ")
+'''
+
+print("Tajul Islam", end=" ")
+print("01303477128")
