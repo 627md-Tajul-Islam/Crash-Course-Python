d12a9f72e8cd4edd4246873e0410006c40346a69
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 12:16:15 -0700

    Else If added

---
+++ If Else.py
@@ -0,0 +1,38 @@
+'''
+mark = 20
+if mark >= 33:
+    print("Pass")
+   print("Program End")
+'''
+
+'''
+# Pass / Fail
+mark = 70
+if mark >= 33 :
+     print("Pass")
+else:
+      print("Fail")
+'''
+
+'''
+# Largest Num
+num1 = 10
+num2 = 5
+
+if num1 > num2 :
+    # print("1 is Great")
+    print(num1)
+else:
+    # print("2 is great")
+    print(num2)
+'''
+
+# even / odd
+
+num = 4
+
+if num%2==0 :
+    print("Even") # জোড় সংখ্যা
+else:
+    print("Odd") # বিজোড় সংখ্যা
+
