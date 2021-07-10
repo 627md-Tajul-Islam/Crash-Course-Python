9a8bfe0a3d6d86b01f4b77d4251f1e5bd14130c3
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 11:31:29 -0700

    Break and continue

---
+++ Break Continue.py
@@ -0,0 +1,19 @@
+# break keyword use korar karone loop theke direct ber hoye asbe and check korbe je condition miltese kina
+
+i = 1
+while i<=100:
+  if i == 20:
+    break
+  print(i)
+  i = i + 1
+print("END OF THE PROGRAM")
+
+# continue use korle loop theke baire jabe na but condition e back asbe.
+i = 1
+while i <= 100:
+    if i == 50:
+        continue
+    print(i)
+    i = i + 1
+
+print("Hello")
