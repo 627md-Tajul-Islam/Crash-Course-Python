e10b76b1549f43fba537bf02e5c24e969d208427
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 12:17:37 -0700

    Letter Grade added

---
+++ Letter Grade.py
@@ -0,0 +1,22 @@
+marks = int(input("Enter Your Marks: "))
+
+if marks >= 90 and marks <=100 :
+    print("A+")
+
+elif marks >=85 and marks <=89 :
+    print("A")
+
+elif marks >=80 and marks <=84 :
+    print("A-")
+
+elif marks >= 77 and marks <= 89 :
+    print("B+")
+
+elif marks >= 73 and marks <= 76 :
+    print("B")
+
+elif marks >= 70 and marks <= 72:
+    print("B-")
+
+else :
+    print("Fail")
