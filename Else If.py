03bb6989f796914b63d2c619f06e0b06ec66a4a9
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 11:54:37 -0700

    Else If added

---
+++ Else If.py
@@ -0,0 +1,44 @@
+# Dont do this babe
+'''
+marks = 74
+
+if marks >=80:
+    print("A+")
+
+if marks >= 70:
+    print("A")
+
+if marks >= 60:
+    print("A-")
+
+if marks >= 50:
+    print("B")
+
+if marks >= 40:
+    print("C")
+
+else:
+    print("Fail")
+'''
+
+# Do this
+
+marks = 74
+
+if marks>=80:
+    print("A+")
+
+elif marks>=70:
+    print("A")
+
+elif marks>=60:
+    print("A-")
+
+elif marks>=50:
+    print("B")
+
+elif marks>=40:
+    print("C")
+
+else:
+    print("Fail")
