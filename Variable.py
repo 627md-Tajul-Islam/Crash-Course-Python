e117579a4c9859e5baf9bcdeb0cd411aa157ffc4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:53:54 -0700

    Variable added

---
+++ Variable.py
@@ -0,0 +1,17 @@
+'''x = 'tajul'
+y = 'dexter devs'
+print(y) '''
+
+#print('Tajul \n Islam')
+#print('Tajul\tIslam')
+
+'''x = 'shakil\'s laptop'
+print(x) '''
+
+name = "Tajul Islam"
+age = 21
+gpa = 4.00
+print("Our New Leader IS" + name )
+print(name + ' ' + "Lives In Jashore")
+print(name + "is Currently" , age  , "Old")
+print(name + " has acheived" , gpa)
