ec06ff8fe691f5ca34826dbb93ba40c1418b75d6
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:10:46 -0700

    Operator added

---
+++ Operator.py
@@ -0,0 +1,35 @@
+'''
+x = 10;
+y = 3;
+z = x * y
+print(z)
+
+x = input("Enter 1st Number")
+y = input("Enter 2nd Number")
+result = x+ y
+print(result)
+
+x = input("1st Number")
+a = int(x)
+y = input("2nd Number")
+b = int(y)
+z = a + b
+print(z)
+
+x = int(input("1st Number"))
+y = int(input("2nd Number"))
+z = x + y
+print(z)
+'''
+x = float(input("1st Number"))
+y = float(input("2nd Number"))
+z = x + y
+print(z)
+
+
+
+
+
+
+
+
