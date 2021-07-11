6d82cab1e259f22223eebc3e68668f51d0ccc78a
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 23:06:19 -0700

    Range added

---
+++ Range.py
@@ -0,0 +1,12 @@
+num = list(range(10)) # range will print the desired number within a range
+print(num) # it means the whole number will be printed
+print(num[2]) # it means it will print the 2nd index number
+
+num = list(range(5,11)) # ami jodi 11 dei taile 10 porjonto asbe,always 1 ta digit kom asbe
+print(num)
+
+num = list(range(2,101,2)) # ekhane 3 ta parameter use kora hoise. 1st ta shuru hobe koi theke,2nd ta kothai sesh hobe,3rd ta difference koto hobe
+print(num)
+
+num = list(range(1,101,2))
+print(num)
