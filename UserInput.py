ac97b9afcec8fb8bf36202b9a8ca4dc44dd5a00d
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 18:11:49 -0700

    UserInput added

---
+++ UserInput.py
@@ -0,0 +1,10 @@
+
+name = input("Enter Name : ")
+age = input("Your Age : ")
+gpa = input("Your GPA : ")
+
+print("Student Information")
+print("---------------")
+print("Name : " + name)
+print("Age : " + age)
+print("GPA : " + gpa)
