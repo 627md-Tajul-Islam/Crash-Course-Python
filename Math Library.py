69383771f47ea06fb6f7f77c43dcea3756aaca41
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:08:44 -0700

    Math library

---
+++ Math Library.py
@@ -0,0 +1,14 @@
+from math import *
+
+'''
+result = max(100,200)
+result = min(100,200)
+result = abs(-4)
+result = pow(2,3)
+result = sqrt(25)
+result = floor(3.7)
+'''
+#print(round(5.8)) # it will return 5 untill its 5.4, after and from 5.5 it will return 6
+
+print(floor(3.7))
+print(ceil(3.7))
