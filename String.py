c46ba4190c4ab9b5b2ef30db319a95aebc645e04
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:13:32 -0700

    String added

---
+++ String.py
@@ -0,0 +1,15 @@
+a = "Data Science"
+#print(a)
+#print(a[0])
+#print(a[2:5])
+# 0 = D
+# 1 = a
+# 2 = t
+# 3 = a
+# 4 = White Space
+#print(len(a))
+#print(a.strip())
+#print(a.upper())
+#print(a.lower())
+#print(a.replace('Data', 'Tajul'))
+print(a.split())
