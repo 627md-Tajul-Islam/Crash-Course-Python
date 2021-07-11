5fa99724f3d6788f1d255b636528556b89c9d95d
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:11:44 -0700

    Operator Version2 added

---
+++ OperatorV2.py
@@ -0,0 +1,48 @@
+#Different Types of Opperator
+
+#Assignment Operator
+a = 15
+b = 10
+'''
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+h = a**b to the power. it means it will multiply the number with the number
+i = a//b  floor. it means it will point out to the lower value
+
+c += a
+print(c)
+'''
+
+#Relational Operator
+'''
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+'''
+#logical Operator
+#c = a>3 and b<2
+
+#unary Operator
+'''
+a = 10
+b = 1
+c = 5
+c = -c
+c = -c
+print(c)
+'''
+'''
+# + -
+a = 10
+b = -a
+print(a)
+print(b)
+'''
+#print(12 % 4)
+
+print(a//b)
