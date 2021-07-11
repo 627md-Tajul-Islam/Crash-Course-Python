0e29f043bba7d46765a1ecf36023ce113de3e951
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:23:21 -0700

    Ternary added

---
+++ Ternary Operator.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 20
+# Ternay means 3
+#Dont do this
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+
+# Do This
+print(num1 if num1>num2 else num2)
+# Even store in a variable
+max = num1 if num1>num2 else num2
+print(max)
