873e3569a6351573e992395c6b738dbb9c3a3341
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-10 17:52:58 -0700

    While Loop added

---
+++ While Loop.py
@@ -0,0 +1,24 @@
+# While lopp has 3 Main Parts
+# 1/ Initialization
+# 2/ while keyword with conditions
+# 3/ update [i=i+1]
+'''
+i = 1
+while i <=10:
+    print(i)
+    i = i + 1
+print("Program End")
+'''
+'''
+i=2
+while i<=100:
+    print(i)
+    i = i + 2
+print("Program ENd")
+'''
+
+i = 1
+while i<= 100:
+    print(i)
+    i = i + 2
+print("done")
