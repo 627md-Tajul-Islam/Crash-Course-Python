3d3652d94a0a06aaf50b16f9d63f9a1c4132c741
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-07-16 11:18:27 -0700

    i have not understood yet

--- For Loop.py
+++ For Loop.py
@@ -1,2 +1,15 @@
 num = [10,20,30,40,50]
-print(num)
+#print(num)
+
+"""
+count = 0
+n = len(num)
+#print(n)
+while count<n :
+    print(num[count])
+    count = count + 1
+"""
+sum = 0
+for x in num:
+    sum = sum + x
+print(sum)
